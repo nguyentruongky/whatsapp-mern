1d516e2caf0a6efc0bdca784d8635eca772894a8
Ky Nguyen <nguyentruongky33@gmail.com>
2021-05-31 02:52:16 +0700

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBH2KKGgTPD_z0XKKS4Uqs9K-Py49dmhwQ',
+    authDomain: 'whatsapp-clone-bea4e.firebaseapp.com',
+    projectId: 'whatsapp-clone-bea4e',
+    storageBucket: 'whatsapp-clone-bea4e.appspot.com',
+    messagingSenderId: '1059671175766',
+    appId: '1:1059671175766:web:f0204243a98df7076101f7',
+}
